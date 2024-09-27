eaf0636e8885f0e3fc496448d5ee1af437d4422a
imergonzalez <imergonzh@gmail.com>
2024-09-26 21:01:12 -0600

    feat: Feature files

---
+++ src/app/features/people/pages/person-list/person-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-person-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './person-list.component.html',
+  styleUrls: ['./person-list.component.css']
+})
+export class PersonListComponent {
+
+}
