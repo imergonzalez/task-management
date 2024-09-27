92ed8b1e85eff7cad590c628108f5783b4bc0318
imergonzalez <imergonzh@gmail.com>
2024-09-26 20:56:18 -0600

    feat: Core files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './shared/components/task-list/task-list.component';
+import { TaskFormComponent } from './shared/components/task-form/task-form.component';
+import { PersonFormComponent } from './shared/components/person-form/person-form.component';
+import { SkillFormComponent } from './shared/components/skill-form/skill-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskFormComponent,
+    PersonFormComponent,
+    SkillFormComponent
   ],
   imports: [
     BrowserModule,
